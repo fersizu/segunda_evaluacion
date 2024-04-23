4c9e37da51364808b5eb2b12ff82f41872063795
PaulinaAR <paulinaarreguinruiz108@gmail.com>
2024-04-23 00:40:28 -0600

    Error imagenes

--- mi_app/urls.py
+++ mi_app/urls.py
@@ -1,2 +1,3 @@
 from django.urls import path
 from mi_app import views
+
--- segunda_evaluacion/settings.py
+++ segunda_evaluacion/settings.py
@@ -16,6 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
+STATIC_DIR = os.path.join(BASE_DIR, "static")
 print(TEMPLATES_DIR)
 
 
@@ -120,6 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
