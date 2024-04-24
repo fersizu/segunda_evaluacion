966bcdb6e5e67efe5c6fcc195b8e7249ed9e20c7
fersizu <fersizu@gmail.com>
2024-04-24 12:39:39 -0600

    commit terminado

--- segunda_evaluacion/settings.py
+++ segunda_evaluacion/settings.py
@@ -122,7 +122,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    STATIC_DIR,
+    #STATIC_DIR,
+    os.path.join(BASE_DIR, 'mi_app/static'),
 ]
 
 # Default primary key field type
